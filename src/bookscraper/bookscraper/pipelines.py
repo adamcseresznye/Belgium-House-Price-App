4163151c9b0c60b4ea4dafbee88c2b928f1cffaa
adamcseresznye <csenye.chem@gmail.com>
2024-01-22 21:54:28 +0100

    mongodb connection established

--- src/bookscraper/bookscraper/pipelines.py
+++ src/bookscraper/bookscraper/pipelines.py
@@ -4,10 +4,44 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import sys
+
+import pymongo
+
+from .items import BookscraperItem
 
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+        return item
+
+
+class MongoDBPipeline:
+    collection = "scrapy_items"
+
+    def __init__(self, mongodb_uri, mongodb_db):
+        self.mongodb_uri = mongodb_uri
+        self.mongodb_db = mongodb_db
+        if not self.mongodb_uri:
+            sys.exit("You need to provide a Connection String.")
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongodb_uri=crawler.settings.get("MONGODB_URI"),
+            mongodb_db=crawler.settings.get("MONGODB_DATABASE", "items"),
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongodb_uri)
+        self.db = self.client[self.mongodb_db]
+        # Start with a clean database
+        self.db[self.collection].delete_many({})
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        data = dict(BookscraperItem(item))
+        self.db[self.collection].insert_one(data)
         return item
--- src/bookscraper/bookscraper/settings.py
+++ src/bookscraper/bookscraper/settings.py
@@ -62,9 +62,10 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    "bookscraper.pipelines.BookscraperPipeline": 300,
-# }
+ITEM_PIPELINES = {
+    #    "bookscraper.pipelines.BookscraperPipeline": 300,
+    "bookscraper.pipelines.MongoDBPipeline": 500
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
