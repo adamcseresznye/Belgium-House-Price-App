e30985f19d3160afc792b323295ffe3329a7f086
adamcseresznye <csenye.chem@gmail.com>
2024-01-30 19:41:01 +0100

    get_house_urls updated

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,69 +5,58 @@ from io import StringIO
 
 import pandas as pd
 import pymongo
-from requests_html import Element, HTMLSession
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
+from requests_html import HTMLSession
 
 
-def get_house_urls(url):
-    options = Options()
-    options.headless = True
-
-    service = Service(GeckoDriverManager().install())
-    driver = webdriver.Firefox(service=service, options=options)
-
-    try:
-        next_page = url
-
+def get_house_urls(url, all_links=None):
+    if all_links is None:
         all_links = []
 
-        while next_page:
-            driver.get(next_page)
-            print("Scraping:", next_page)
+    session = HTMLSession(
+        browser_args=[
+            "--no-sandbox",
+            "--user-agent=Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1",
+        ]
+    )
+    r = session.get(url)
+    r.html.render(timeout=15)
 
-            elements = driver.find_elements(By.CSS_SELECTOR, ".search-results a")
-            links = [element.get_attribute("href") for element in elements]
-            filtered_links = [
-                link
-                for link in links
-                if link and "www.immoweb.be/en/classified/house/for-sale" in link
-            ]
+    elements = r.html.find(".search-results a")
+    links = [element.attrs["href"] for element in elements if "href" in element.attrs]
+    filtered_links = [
+        link
+        for link in links
+        if link and "www.immoweb.be/en/classified/house/for-sale" in link
+    ]
 
-            all_links.extend(filtered_links)
+    all_links.extend(filtered_links)
 
-            # Find the link to the next page
-            next_page_elements = driver.find_elements(
-                By.CSS_SELECTOR, "a.pagination__link--next"
-            )
-            if next_page_elements:
-                next_page = next_page_elements[0].get_attribute("href")
-            else:
-                break
-    finally:
-        driver.quit()
-
-    return all_links
+    next_page = r.html.next()
+    if next_page:
+        print("Next page:", next_page)
+        return get_house_urls(next_page, all_links)
+    else:
+        return all_links
 
 
 def get_house_data(url):
-    session = HTMLSession()
-    r = session.get(url)
-    r.html.render(sleep=5)
+    try:
+        session = HTMLSession()
+        r = session.get(url)
+        r.html.render(timeout=15)
 
-    individual_ad = pd.concat(pd.read_html(StringIO(r.text))).dropna().set_index(0)
+        individual_ad = pd.concat(pd.read_html(StringIO(r.text))).dropna().set_index(0)
 
-    # Extract list_price and province
-    list_price = r.html.find("p.classified__price span.sr-only", first=True)
-    if list_price:
-        individual_ad.loc["list_price", 1] = list_price.text.strip()
+        # Extract list_price and province
+        list_price = r.html.find("p.classified__price span.sr-only", first=True)
+        if list_price:
+            individual_ad.loc["list_price", 1] = list_price.text.strip()
 
-    individual_ad.loc["day_of_retrieval", 1] = str(date.today())
-    individual_ad.loc["ad_url", 1] = url
-    return individual_ad.squeeze().to_dict()
+        individual_ad.loc["day_of_retrieval", 1] = str(date.today())
+        individual_ad.loc["ad_url", 1] = url
+        return individual_ad.squeeze().to_dict()
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
@@ -85,5 +74,6 @@ if __name__ == "__main__":
         print("Working on:", url)
         result = get_house_data(url)
         db.BE_houses.insert_one(result)
+        time.sleep(1)
 
     client.close()
