572944520ad3038f3b4866d83c855ffd47de3ac9
adamcseresznye <csenye.chem@gmail.com>
2024-01-31 21:11:31 +0100

    request-html added

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,12 +4,16 @@ import time
 from datetime import date
 from io import StringIO
 
+import numpy as np
 import pandas as pd
 import pymongo
-from requests_html import HTMLSession
+from requests_html import AsyncHTMLSession, HTMLSession
 
 
-def get_house_urls(url, all_links=None):
+def get_house_urls(
+    url="https://www.immoweb.be/en/search/house/for-sale?countries=BE&page=1&orderBy=relevance",
+    all_links=None,
+):
     if all_links is None:
         all_links = []
 
@@ -57,43 +61,6 @@ def get_house_data(url):
         print(f"An error occurred: {e}")
 
 
-def process_item(data):
-    # List of columns to keep
-    columns_to_keep = [
-        "ad_url",
-        "price",
-        "day_of_retrieval",
-        "zip_code",
-        "energy_class",
-        "primary_energy_consumption",
-        "bedrooms",
-        "tenement_building",
-        "living_area",
-        "surface_of_the_plot",
-        "bathrooms",
-        "double_glazing",
-        "number_of_frontages",
-        "building_condition",
-        "toilets",
-        "heating_type",
-        "construction_year",
-    ]
-
-    return (
-        data.to_frame()
-        .rename(index=lambda x: x.replace(" ", "_").lower())
-        .filter(items=columns_to_keep, axis="index")
-        .transpose()
-        .assign(
-            price=lambda x: x["price"]
-            .str.extract(r"(\d+,\d+)", expand=False)
-            .str.replace(",", "")
-            .astype(int)
-        )
-        .transpose()
-    )
-
-
 def convert_zip_to_province(value):
     # data from https://www.spotzi.com/en/data-catalog/categories/postal-codes/belgium/
     if value is None:
@@ -124,8 +91,107 @@ def convert_zip_to_province(value):
     return None
 
 
+class DataCleaner:
+    def __init__(self, data):
+        self.original_data = data
+        self.columns_to_keep = [
+            "ad_url",
+            "price",
+            "day_of_retrieval",
+            "zip_code",
+            "energy_class",
+            "primary_energy_consumption",
+            "bedrooms",
+            "tenement_building",
+            "living_area",
+            "surface_of_the_plot",
+            "bathrooms",
+            "double_glazing",
+            "number_of_frontages",
+            "building_condition",
+            "toilets",
+            "heating_type",
+            "construction_year",
+        ]
+
+    def reformat_headers(self, data):
+        return data.transpose().rename(columns=lambda x: x.replace(" ", "_").lower())
+
+    def add_missing_columns(self, data):
+        for column in self.columns_to_keep:
+            if column not in data.columns:
+                data[column] = "missing"
+        return data
+
+    def filter_columns(self, data):
+        return data[self.columns_to_keep]
+
+    def reformat_entries(self, data):
+        return (
+            data.assign(
+                price=lambda x: pd.to_numeric(
+                    x["price"]
+                    .str.extract(r"(\d+,\d+)", expand=False)
+                    .str.replace(",", ""),
+                    errors="coerce",
+                ),
+                province=lambda x: x["zip_code"].apply(convert_zip_to_province),
+                energy_class=lambda x: x["energy_class"].str.strip(),
+                primary_energy_consumption=lambda x: pd.to_numeric(
+                    x["primary_energy_consumption"]
+                    .str.extract("(\d+)", expand=False)
+                    .str.replace(",", ""),
+                    errors="coerce",
+                ),
+                bedrooms=lambda x: pd.to_numeric(
+                    x["bedrooms"].str.extract("(\d+)", expand=False), errors="coerce"
+                ),
+                tenement_building=lambda x: x["tenement_building"].str.strip(),
+                living_area=lambda x: pd.to_numeric(
+                    x["living_area"]
+                    .str.extract("(\d+)", expand=False)
+                    .str.replace(",", ""),
+                    errors="coerce",
+                ),
+                surface_of_the_plot=lambda x: pd.to_numeric(
+                    x["surface_of_the_plot"]
+                    .str.extract("(\d+)", expand=False)
+                    .str.replace(",", ""),
+                    errors="coerce",
+                ),
+                bathrooms=lambda x: pd.to_numeric(
+                    x["bathrooms"].str.extract("(\d+)", expand=False), errors="coerce"
+                ),
+                double_glazing=lambda x: x["double_glazing"].str.strip(),
+                number_of_frontages=lambda x: pd.to_numeric(
+                    x["number_of_frontages"].str.extract("(\d+)", expand=False),
+                    errors="coerce",
+                ),
+                building_condition=lambda x: x["building_condition"].str.strip(),
+                toilets=lambda x: pd.to_numeric(
+                    x["toilets"].str.extract("(\d+)", expand=False), errors="coerce"
+                ),
+                heating_type=lambda x: x["heating_type"].str.strip(),
+                construction_year=lambda x: pd.to_numeric(
+                    x["construction_year"].str.extract("(\d+)", expand=False),
+                    errors="coerce",
+                ),
+            )
+            .transpose()
+            .squeeze()
+            .to_dict()
+        )
+
+    def process_item(self):
+        return (
+            self.original_data.pipe(self.reformat_headers)
+            .pipe(self.add_missing_columns)
+            .pipe(self.filter_columns)
+            .pipe(self.reformat_entries)
+        )
+
+
 if __name__ == "__main__":
-    """
     # get links to houses
     urls = get_house_urls(
         "https://www.immoweb.be/en/search/house/for-sale?countries=BE&page=332&orderBy=relevance"
@@ -139,60 +205,33 @@ if __name__ == "__main__":
     for url in urls:
         print("Working on:", url)
         result = get_house_data(url)
-        db.BE_houses.insert_one(result)
+
+        data_cleaner = DataCleaner(result)
+        processed_dict = data_cleaner.process_item()
+
+        db.BE_houses.insert_one(processed_dict)
         time.sleep(1)
 
     client.close()
-    """
 
-    d = {
-        "Available as of": "After signing the deed",
-        "Available date": "April 1 2024 - 12:00 AM",
-        "Construction year": "1900",
-        "Building condition": "Good",
-        "Street frontage width": "30 m",
-        "Number of frontages": "4",
-        "Outdoor parking spaces": "10",
-        "Surroundings type": "Isolated",
-        "Living area": "180  m² square meters",
-        "Living room surface": "44  m² square meters",
-        "Dining room": "Yes",
-        "Kitchen type": "Installed",
-        "Kitchen surface": "12  m² square meters",
-        "Bedrooms": "4",
-        "Bedroom 1 surface": "10  m² square meters",
-        "Bedroom 2 surface": "10  m² square meters",
-        "Bedroom 3 surface": "13  m² square meters",
-        "Bedroom 4 surface": "17  m² square meters",
-        "Bathrooms": "1",
-        "Toilets": "2",
-        "Office surface": "8  m² square meters",
-        "Office": "Yes",
-        "Professional space surface": "93  m² square meters",
-        "Basement surface": "25  m² square meters",
-        "Furnished": "No",
-        "Surface of the plot": "2435  m²  square meters",
-        "Connection to sewer network": "Connected",
-        "Garden surface": "2000  m²  square meters",
-        "Terrace surface": "30  m²  square meters",
-        "Primary energy consumption": "329  kWh/m²  kilowatt hour per square meters",
-        "Energy class": "D",
-        "Reference number of the EPC report": "20231117000919",
-        "CO₂ emission": "81 kg CO₂/m²",
-        "Yearly theoretical total energy consumption": "77396 kWh/year",
-        "Heating type": "Fuel oil",
-        "Planning permission obtained": "Yes",
-        "Total ground floor buildable": "300  m²  square meters",
-        "Subdivision permit": "Yes",
-        "Latest land use designation": "Living area (residential, urban or rural)",
-        "Price": "€ 400,000  400000 €",
-        "Cadastral income": "€ 468  468 €",
-        "Tenement building": "No",
-        "Address": "Chaussée de Tirlemont 15/1 4260  - Braives",
-        "Website": "http://www.immodemarneffe.be",
-        "External reference": "5619624",
-        "day_of_retrieval": "2024-01-30",
-        "ad_url": "https://www.immoweb.be/en/classified/house/for-sale/braives/4260/10985247",
-        "zip_code": "4260",
-    }
-    print(process_item(pd.Series(d)))
+    urls = [
+        "https://www.immoweb.be/en/classified/house/for-sale/daverdisse/6929/10909568",
+        "https://www.immoweb.be/en/classified/house/for-sale/koekelare/8680/10771155",
+        "https://www.immoweb.be/en/classified/house/for-sale/houthalen-helchteren/3530/10987558",
+        "https://www.immoweb.be/en/classified/house/for-sale/tienen/3300/11022381",
+        "https://www.immoweb.be/en/classified/house/for-sale/huy/4500/11021927",
+        "https://www.immoweb.be/en/classified/house/for-sale/noirefontaine/6831/11022526",
+        "https://www.immoweb.be/en/classified/house/for-sale/ochamps/6890/10798426",
+        "https://www.immoweb.be/en/classified/house/for-sale/sint-amandsberg/9040/11012090",
+        "https://www.immoweb.be/en/classified/house/for-sale/merelbeke/9820/11058929",
+        "https://www.immoweb.be/en/classified/house/for-sale/marbais/1495/11054219",
+    ]
+
+    start = time.perf_counter()
+    work(urls)
+
+    end = time.perf_counter()
+    print(f"Time taken: {end - start}")
+
+# Time taken: 42.20484900000156
+# time taken: 13.7827954999957
